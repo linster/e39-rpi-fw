e6c5aa1038f34ee6fccbf34ba0ee7c5503f1ef78
Stefan Martynkiw <linster247@gmail.com>
2023-03-31 21:54:43 -0400

    We can make protos with nanopb, but really badly.

--- configuration/ConfigurationManager.cpp
+++ configuration/ConfigurationManager.cpp
@@ -6,5 +6,9 @@
 
 namespace pico {
     namespace config {
+
+//        https://github.com/nanopb/nanopb
+//        https://github.com/nanopb/nanopb_cpp
+//        https://jpa.kapsi.fi/nanopb/docs/index.html
     } // pico
 } // config
