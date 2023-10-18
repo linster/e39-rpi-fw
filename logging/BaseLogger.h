4d9afc35c4c83d572ea24ab222d517dbaa740f57
Stefan Martynkiw <linster247@gmail.com>
2023-10-18 09:04:28 -0400

    Add =0 on base class methods for pure virtual class

--- logging/BaseLogger.h
+++ logging/BaseLogger.h
@@ -13,11 +13,11 @@ namespace pico {
         class BaseLogger {
 
         public:
-            virtual void d(std::string tag, std::string message);
-            virtual void i(std::string tag, std::string message);
-            virtual void w(std::string tag, std::string message);
-            virtual void e(std::string tag, std::string message);
-            virtual void wtf(std::string tag, std::string message);
+            virtual void d(std::string tag, std::string message) =0;
+            virtual void i(std::string tag, std::string message) =0;
+            virtual void w(std::string tag, std::string message) =0;
+            virtual void e(std::string tag, std::string message) =0;
+            virtual void wtf(std::string tag, std::string message) =0;
 
             //TODO have another level for logging about writing bytes to IBUS,
             //TODO so we don't get an infinite loop when we write one log statement to Ibus.
