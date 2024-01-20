16ab136b3d5bb26aacc4a1704d4b26b6d6f48fa5
Stefan Martynkiw <linster247@gmail.com>
2024-01-20 16:46:52 -0500

    Finish the bus topology manager

--- ibus/dma/SingleCoreDmaManager.cpp
+++ ibus/dma/SingleCoreDmaManager.cpp
@@ -153,13 +153,20 @@ namespace pico::ibus::dma {
 
 
         if (busTopologyManager->getBusToplogy() == topology::BusTopology::SLED_NO_PI) {
+            //This will cause printf's from the StdioPrintFlogger to also show up
+            //on the UART, as well as writes from this class.
             stdio_uart_init_full(uart1, 115200, UART1_PICOPROBE_TX, UART1_PICOPROBE_RX);
         }
 
         if (busTopologyManager->getBusToplogy() == topology::BusTopology::CAR_WITH_PI) {
-            //TODO This might depend on CMake settings to make sure that usb stdio is enabled.
+            //CMake flags have caused only the USB stdio to be enabled, but we still
+            //have to enable stdio on the usb port for debugging.
             stdio_init_all();
         }
+
+        //TODO now what we need to do is make sure all our write outs to UART1 are actually going
+        //TODO to the usb stdout.
+
         //Not sure if this helps.
         //sleep_ms(200);
 
--- ibus/dma/topology/PicoBusTopology.h
+++ ibus/dma/topology/PicoBusTopology.h
@@ -20,19 +20,22 @@ namespace pico::ibus::topology {
          *          |                            ^
          *          |                            | (swd)
          *          v                            v
-         *      +--------+                  +--------+       +---------+
-         *      | Laptop |                  |  Pico  |<=====>| MCP2020 | (UART0, 9600-8E1, IBUS raw)
-         *      +--------+                  +--------+       +---------+
+         *      +--------+                     +--------+       +---------+
+         *      | Laptop |<-(usb stdio logs)->|  Pico  |<=====>| MCP2020 | (UART0, 9600-8E1, IBUS raw)
+         *      +--------+                    +--------+       +---------+
          *                                      ^^
-         *                                      ||<......................(USB stdio, 9600-8E1, IBUS raw)
+         *                                      ||<........................(UART1, 9600-8E1, IBUS raw)
+         *                                      ||
+         *                       (AdaFruit 954 Serial <-> USB Adapter)
+         *                                      ||
          *                                      vv
          *                                  +--------+
          *                                  |  Rpi  |
          *                                  +-------+
          *
-         * We can't use UART1 to talk to the Rpi because when the Rpi is in display parallel mode
+         * We can't use UART1 to talk to the Rpi directly because when the Rpi is in display parallel mode
          * to get VGA-out, there aren't enough GPIO pins we can use (and they're all disabled for SPI
-         * too). So, we run another USB cable from the pico to the pi.
+         * too). So, we run another USB cable from the mainboard to the pi.
          */
         CAR_WITH_PI,
 
