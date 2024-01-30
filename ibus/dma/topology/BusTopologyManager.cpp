f2d5d84de497fd12d3f803903a170ccd7c297b09
Stefan Martynkiw <linster247@gmail.com>
2024-01-29 23:05:12 -0500

    As a scripting language, CMake is garbage.

--- ibus/dma/topology/BusTopologyManager.cpp
+++ ibus/dma/topology/BusTopologyManager.cpp
@@ -8,15 +8,15 @@ namespace pico::ibus::topology {
     BusTopology BusTopologyManager::getBusToplogy() {
         #ifdef CMAKE_BUS_TOPOLOGY
 
-            #ifdef CMAKE_BUS_TOPOLOGY_CAR_WITH_PI
+            #if (CMAKE_BUS_TOPOLOGY == CMAKE_BUS_TOPOLOGY_CAR_WITH_PI)
                 return BusTopology::CAR_WITH_PI;
             #endif
 
-            #ifdef CMAKE_BUS_TOPOLOGY_SLED_NO_PI
+            #if (CMAKE_BUS_TOPOLOGY == CMAKE_BUS_TOPOLOGY_SLED_NO_PI)
                 return BusTopology::SLED_NO_PI;
             #endif
 
-            #ifdef CMAKE_BUS_TOPOLOGY_SLED_LAPTOP_HMI
+            #if (CMAKE_BUS_TOPOLOGY == CMAKE_BUS_TOPOLOGY_SLED_LAPTOP_HMI)
                 return BusTopology::SLED_LAPTOP_HMI;
             #endif
         #else
